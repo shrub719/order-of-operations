9ceb015d0e025894c98ea7f7ab3b373608cf6b0e
shrub <shrub719@gmail.com>
2026-07-24 00:32:26 +0100

    add test1 level

--- src/gameplay/board.gd
+++ src/gameplay/board.gd
@@ -56,7 +56,7 @@ func _ready() -> void:
 			row.append(null)
 		drawer_block_pointers.append(row)
 	
-	load_level("dev2")
+	load_level("test1")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
