154fcedb6f89dd12fdcb19e672990942403bb73c
Fortress Nebula <prasun.nandan0909@gmail.com>
2026-07-25 20:21:40 +0100

    level container actually has a list of levels

--- src/gameplay/Settings.gd
+++ src/gameplay/Settings.gd
@@ -18,3 +18,19 @@ var level_index = 9
 
 func level():
 	return level_order[level_index]
+
+func level_name_from_file(id):
+	var file = FileAccess.open("res://src/gameplay/levels/" + id, FileAccess.READ)
+	var text = file.get_as_text()
+	# FUCK OFF WINDOWS
+	text = text.remove_char(ord("\r"))
+
+	var content = []
+	for section in text.split("\n\n"):
+		var current_section = []
+		for line in section.split("\n"):
+			if line == "": continue
+			current_section.append(line.split(" "))	
+		content.append(current_section)
+
+	return " ".join(content[0][0])
---
+++ src/gui/levels_container.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+var button_scene: PackedScene = preload("res://src/gui/level_button.tscn")
+
+func _ready() -> void:
+	# generate all level buttons
+	for idx in range(len(Settings.level_order)):
+		var button: Button = button_scene.instantiate()
+		button.text = str(idx + 1) + ". " + Settings.level_name_from_file(Settings.level_order[idx])
+		add_child(button)
