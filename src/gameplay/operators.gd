bf3010a5f42b14dd814d3df67918d06496a34c8b
shrub <shrub719@gmail.com>
2026-07-25 16:00:48 +0100

    fix typo

--- src/gameplay/operators.gd
+++ src/gameplay/operators.gd
@@ -7,7 +7,7 @@ const MULTIPLICATION := 12
 const SUBTRACTION := 13
 const SWAPIFICATION := 14
 const GLYPH_EQUALITY := 15
-const GLYPH_SWAPIFICIATION := 16
+const GLYPH_SWAPIFICATION := 16
 
 func get_numerical_neighbours(left, right, up, down):
 	var neighbours = []
