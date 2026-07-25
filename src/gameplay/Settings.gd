912c386cba8ec608edec8b003d095ca658190e9e
shrub <shrub719@gmail.com>
2026-07-25 15:01:23 +0100

    add some more levels

--- src/gameplay/Settings.gd
+++ src/gameplay/Settings.gd
@@ -2,17 +2,19 @@ extends Node
 
 var can_drag_blocks := true
 var level_order = [
-	"playground",
+	#"playground",
 	"test1",
 	"test2",
-	"train",
 	"cascade",
 	"simulswap-1",
 	"simulswap-2",
 	"simulswap-3",
+	"train",
+	"buckle",
+	"shift-1",
+	"shift-2",
 ]
 var level_index = 0
 
 func level():
 	return level_order[level_index]
-
