469d73a220b05edfe8a93e06678a79738fec3e89
Fortress Nebula <prasun.nandan0909@gmail.com>
2026-07-26 00:47:22 +0100

    play button is now a oneshot
    (ONESHOT MENTIONED?? PEAK GAME??)

--- src/gui/play_button.gd
+++ src/gui/play_button.gd
@@ -1,5 +1,9 @@
 extends TextureButton
 
+var been_pressed := false
+
 func _on_button_down() -> void:
+	if been_pressed: return
+	been_pressed = true
 	get_node("/root/Sfxmanager").click(0)
 	get_node("/root/Transition").transition_to("res://src/gui/level_select.tscn")
